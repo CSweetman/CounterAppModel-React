5b77a95bd1fa07b2422b0d2f5a949b43586cf3d2
Christopher Sweetman <chrisjdsweetman@gmail.com>
2020-09-23 21:09:39 -0500

    Pushed Counters state upp, Navbar keeps track of counters Changes: Pushed the state of Counters into the App.js file. Counters communicates with Counter and app.js by passing props. Update NavBar to count how many counters there are w/ value >1.
    Security Concerns:
    N/A

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
-    state = {}
+
     render() {
         return (<nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Navbar
+                Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+            </a>
         </nav>);
     }
 }
